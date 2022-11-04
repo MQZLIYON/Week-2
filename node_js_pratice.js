a8045b4df022d939c226f10bb5b35b24048c81c4
Mushfiquzzaman Liyon <farhanliyon308@gmail.com>
2022-11-04 19:08:09 +0600

    Update

--- node_js_pratice.js
+++ node_js_pratice.js
@@ -1,10 +1,22 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-    response.end("Hello World")
+    if (request.url == '/'){
+        response.writeHead(200,{'Content':'text/html'});
+        response.write('<h1>This is home Page</h1>');
+        response.end();
+    }
+    else if (request.url == '/about'){
+        response.writeHead(200,{'Content':'text/html'});
+        response.write('<h1>This is about page</h1>');
+        response.end();
+    }
+    else if (request.url == '/contact'){
+        response.writeHead(200,{'Content':'text/html'});
+        response.write('<h1>This is contact</h1>');
+        response.end();
+    }
 });
 
-// set server port
 server.listen(5050);
-
-console.log("Server run success");  
+console.log("Server run success");
