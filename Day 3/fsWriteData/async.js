ae1564085a529a2f7978b324ded65c8985a963fa
Mushfiquzzaman Liyon <farhanliyon308@gmail.com>
2022-11-05 10:59:35 +0600

    Update

--- Day 3/fsWriteData/async.js
+++ Day 3/fsWriteData/async.js
@@ -1,19 +1,24 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-    fs.writeFile('demo.txt','Hello World',function (error) {
-        if (error){
-            res.writeHead(200,{'Content-Type':'txt/html'});
-            res.write('<h1>Error</h1>')
-            res.end();
-        }
-        else {
-            res.writeHead(200,{'Content-Type':'txt/html'});
-            res.write('<h1>Success</h1>')
-            res.end();
-        }
-    })
+var server = http.createServer(function (req,res) {
+    if (req.url == '/'){
+        fs.writeFile('demo.txt','Hey!!!!!!!!!! I am Liyon',function (error) {
+            if (error){
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write("Error");
+                res.end();
+            }
+            else {
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write("Success");
+                res.end();
+            }
+        });
+
+    }
 });
+
 server.listen(5050);
+
 console.log("Server run success");
